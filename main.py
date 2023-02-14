524d3d69706178d78130aa0722bb228afe6815c3
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2023-02-14 12:11:48 +0000

    Added excel

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import pandas as pd
+
+import numpy as np
+import networkx as nx
+
+def function(var1, var2):
+
+    return 
+
+def main():
+
+    
+
+    return
+
+if __name__ == '__main__':
+    #sys.argv = ["programName.py","--input","test.txt","--output","tmp/test.txt"]
+    main() 
